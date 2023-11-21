06aa08a37572200eee4783789abce2cfefe40292
Ikshita Puri <ikshitapuri@berkeley.edu>
2023-11-21 17:21:35 +0000

    [feat] added profile page view

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,4 +4,10 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+
+  def show
+    #look up representative by id (ocdid?)
+    #id = params[:id]
+    #@representative = Representative.find(id)
+  end
 end
