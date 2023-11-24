1bf977f8b00ae9d7e7884f95034a57b06304dcc2
Leo Wang <leiw@berkeley.edu>
2023-11-24 00:51:14 +0000

    Added db migration files, need to run db:migrate

---
+++ db/migrate/20231124004448_add_more_details_to_representatives.rb
@@ -0,0 +1,9 @@
+class AddMoreDetailsToRepresentatives < ActiveRecord::Migration[5.2]
+  def change
+    add_column :representatives, :address, :string
+    add_column :representatives, :city, :string
+    add_column :representatives, :zip, :string
+    add_column :representatives, :party, :string
+    add_column :representatives, :photo, :string
+  end
+end
