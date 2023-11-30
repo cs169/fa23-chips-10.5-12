102d21dad04bc0fa75e60df197e857dc597609b6
Connor Bernard <connorbernard@berkeley.edu>
2023-11-30 01:26:14 -0800

    feat: add mapping display name to controller

--- app/controllers/campaign_finance_controller.rb
+++ app/controllers/campaign_finance_controller.rb
@@ -3,5 +3,16 @@
 require_relative '../adapters/propublica_adapter'
 
 class CampaignFinanceController < ApplicationController
-  @cycles = (PropublicaAdapter::START_CYCLE..PropublicaAdapter::CURRENT_CYCLE).step(2).to_a
+  @cycles              = (PropublicaAdapter::START_CYCLE..PropublicaAdapter::CURRENT_CYCLE).step(2).to_a
+  @categories_to_names = {
+    candidateLoan:      'Candidate Loan',
+    contributionTotal:  'Contribution Total',
+    debtsOwed:          'Debts Owed',
+    disbursementsTotal: 'Disbursements Total',
+    endCash:            'End Cash',
+    individualTotal:    'Individual Total',
+    pacTotal:           'PAC Total',
+    receiptsTotal:      'Receipts Total',
+    refundTotal:        'Refund Total'
+  }
 end
