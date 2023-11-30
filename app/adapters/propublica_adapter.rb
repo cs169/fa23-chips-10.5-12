466f25bbbcb16f01310936086404b70fc9f43fe0
Connor Bernard <connorbernard@berkeley.edu>
2023-11-30 01:25:58 -0800

    feat: add mapping to value to adapter

--- app/adapters/propublica_adapter.rb
+++ app/adapters/propublica_adapter.rb
@@ -5,4 +5,15 @@ module PropublicaAdapter
   API_BASE_URL  = "https://api.propublica.org/campaign-finance/#{self::API_VERSION}/"
   START_CYCLE   = 1996
   CURRENT_CYCLE = 2018
+  CATEGORIES    = {
+    candidateLoan:      'candidate-loan',
+    contributionTotal:  'contribution-total',
+    debtsOwed:          'debts-owed',
+    disbursementsTotal: 'disbursements-total',
+    endCash:            'end-cash',
+    individualTotal:    'individual-total',
+    pacTotal:           'pac-total',
+    receiptsTotal:      'receipts-total',
+    refundTotal:        'refund-total'
+  }.freeze
 end
