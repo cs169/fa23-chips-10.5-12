aeadda0995b780d5b387477544ddd4fc9a68d86f
Connor <78193645+Connor-Bernard@users.noreply.github.com>
2023-11-30 16:37:11 -0800

    [FEAT] Set Up Campaign Variable (#35)
    "_FIPS_" note: this PR fails pronto.  See PR for details on why that failure is being marked as `will not fix`.
    
    * fix: prevent intermittent test failure
    
    * chore: resolve lint errors
    
    * feat: set up cycle variable
    
    * test: add test coverage
    
    * test: add tests for categories
    
    * feat: add mapping to value to adapter
    
    * feat: add mapping display name to controller

--- spec/adapters/propublica_adapter_spec.rb
+++ spec/adapters/propublica_adapter_spec.rb
@@ -15,5 +15,43 @@ describe PropublicaAdapter do
     it 'sets the start cycle to an even number' do
       expect(described_class::START_CYCLE.even?).to be(true)
     end
+
+    describe 'CATEGORY' do
+      it 'properly maps candidateLoan' do
+        expect(described_class::CATEGORIES[:candidateLoan]).to eq('candidate-loan')
+      end
+
+      it 'properly maps contributionTotal' do
+        expect(described_class::CATEGORIES[:contributionTotal]).to eq('contribution-total')
+      end
+
+      it 'properly maps debtsOwed' do
+        expect(described_class::CATEGORIES[:debtsOwed]).to eq('debts-owed')
+      end
+
+      it 'properly maps disbursementsTotal' do
+        expect(described_class::CATEGORIES[:disbursementsTotal]).to eq('disbursements-total')
+      end
+
+      it 'properly maps endCash' do
+        expect(described_class::CATEGORIES[:endCash]).to eq('end-cash')
+      end
+
+      it 'properly maps individualTotal' do
+        expect(described_class::CATEGORIES[:individualTotal]).to eq('individual-total')
+      end
+
+      it 'properly maps pacTotal' do
+        expect(described_class::CATEGORIES[:pacTotal]).to eq('pac-total')
+      end
+
+      it 'properly maps receiptsTotal' do
+        expect(described_class::CATEGORIES[:receiptsTotal]).to eq('receipts-total')
+      end
+
+      it 'properly maps refundTotal' do
+        expect(described_class::CATEGORIES[:refundTotal]).to eq('refund-total')
+      end
+    end
   end
 end
