70864bea1bfa790c90890cd0424149f5d0af5218
Connor Bernard <connorbernard@berkeley.edu>
2023-12-01 01:51:27 -0800

    feat: configure adapter and add method to controller

--- app/adapters/propublica_adapter.rb
+++ app/adapters/propublica_adapter.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'json'
+
 module PropublicaAdapter
   API_VERSION   = 'v1'
   API_BASE_URL  = "https://api.propublica.org/campaign-finance/#{self::API_VERSION}/"
@@ -16,4 +19,20 @@ module PropublicaAdapter
     receiptsTotal:      'receipts-total',
     refundTotal:        'refund-total'
   }.freeze
+
+  def get(route)
+    uri = URI.parse("#{API_BASE_URL}#{route}.json")
+    req = Net::HTTP::Get.new(uri)
+    req['X-API-Key'] = Rails.application.credentials[:PROPUBLICA_API_KEY]
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(req) }
+    res.body
+  end
+
+  class Candidate
+    include PropublicaAdapter
+    def get_top_20_by_cycle_and_category(cycle, category)
+      route = "#{cycle}/candidates/leaders/#{CATEGORIES[category.to_sym]}"
+      JSON.parse(get(route))['results']
+    end
+  end
 end
