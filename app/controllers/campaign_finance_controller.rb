6a757b39d21d5bcaa830149cf09763adc538dce3
Leo Wang <leiw@berkeley.edu>
2023-12-01 06:12:31 +0000

    Finished front page for campaign

--- app/controllers/campaign_finance_controller.rb
+++ app/controllers/campaign_finance_controller.rb
@@ -3,16 +3,31 @@
 require_relative '../adapters/propublica_adapter'
 
 class CampaignFinanceController < ApplicationController
-  @cycles              = (PropublicaAdapter::START_CYCLE..PropublicaAdapter::CURRENT_CYCLE).step(2).to_a
-  @categories_to_names = {
-    candidateLoan:      'Candidate Loan',
-    contributionTotal:  'Contribution Total',
-    debtsOwed:          'Debts Owed',
-    disbursementsTotal: 'Disbursements Total',
-    endCash:            'End Cash',
-    individualTotal:    'Individual Total',
-    pacTotal:           'PAC Total',
-    receiptsTotal:      'Receipts Total',
-    refundTotal:        'Refund Total'
-  }
+  before_action :set_cycles_and_categories, only: [:index, :search]
+
+  def set_cycles_and_categories
+    @cycles              = (PropublicaAdapter::START_CYCLE..PropublicaAdapter::CURRENT_CYCLE).step(2).sort.to_a
+    @categories_to_names = {
+      candidateLoan:      'Candidate Loan',
+      contributionTotal:  'Contribution Total',
+      debtsOwed:          'Debts Owed',
+      disbursementsTotal: 'Disbursements Total',
+      endCash:            'End Cash',
+      individualTotal:    'Individual Total',
+      pacTotal:           'PAC Total',
+      receiptsTotal:      'Receipts Total',
+      refundTotal:        'Refund Total'
+    }
+  end
+
+  def index
+    @cycle = params[:cycle]
+    @category = params[:category]
+  end
+
+  def search
+    @cycle = params[:cycle]
+    @category = params[:category]
+    @top_candidates = CampaignFinance.top_candidates(@category, @cycle)
+  end
 end
--- app/models/campaign_finance.rb
+++ app/models/campaign_finance.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../adapters/propublica_adapter'
+require 'net/http'
+require 'uri'
+require 'json'
+
 class CampaignFinance < ApplicationRecord
+  def self.top_candidates(category, cycle)
+    #raw data to get it display
+    @selected_cycle = '2022'
+    @selected_category = 'Contribution Total'
+    @top_candidates = [
+      { name: 'John Doe', party: 'Democratic', contributions: 50000 },
+      { name: 'Jane Smith', party: 'Republican', contributions: 45000 },].sort_by { |candidate| candidate[:name] }
+  end
 end
