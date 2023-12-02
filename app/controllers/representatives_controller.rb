c436ce54829e06cab6a0c20f88a15fbc65ac2c89
Ikshita Puri <61445592+ikshitapuri@users.noreply.github.com>
2023-12-01 23:02:00 -0800

    Feature profile page/11 (#31)
    * setup
    
    * [feat] added profile page view
    
    * feature rep profile page
    
    * profile page modified tests
    
    * profile-page fixed errors
    
    * lint errors fix
    
    * fixed errors
    
    * testing fixed
    
    * lint errors fixed
    
    * fixed test
    
    * resoved comments
    
    * checked out files

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,4 +4,11 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+
+  def show
+    @representative = Representative.find(params[:id])
+    officials = Representative.get_representatives_by_ocdid(@representative.ocdid)
+    @details = officials.find { |official| official.name == @representative.name }
+    @photo_url_exists = @details.respond_to?(:photo_url) && @details.photo_url
+  end
 end
---
+++ spec/controllers/representatives_controller_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spec_helper'
+
+describe RepresentativesController do
+  describe '#index' do
+    it 'assigns array of all representatives to @representatives' do
+      representative = Representative.create({ name: 'Kamala Harris', ocdid: '1', title: '' })
+      get :index
+      expect(assigns(:representatives)).to eq([representative])
+    end
+  end
+
+  describe '#show' do
+    it 'assigns photo_url_exists to false' do
+      representative = create(:representative, name: 'Joe Diggs')
+      official = create(:representative, name: 'Joe Diggs')
+      allow(Representative).to receive(:get_representatives_by_ocdid).and_return([official])
+      get :show, params: { id: representative.id }
+      expect(assigns(:photo_url_exists)).to be_falsy
+    end
+  end
+end
