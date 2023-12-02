f5b8dd9e5828cb237cf5f0116f613fbf98711934
Ikshita Puri <ikshitapuri@berkeley.edu>
2023-12-02 00:41:55 +0000

    testing fixed

--- spec/controllers/representatives_controller_spec.rb
+++ spec/controllers/representatives_controller_spec.rb
@@ -11,28 +11,4 @@ describe RepresentativesController do
       expect(assigns(:representatives)).to eq([@representative])
     end
   end
-
-  describe '#show' do
-    before do
-      @representative = Representative.create({ name: 'Kamala Harris', ocdid: '1', title: '' })
-      @officials = [double('Representative', name: 'Kamala Harris', photo_url: 'url')]
-    end
-    
-    it 'finds and assigns the correct representative to @representative' do
-      get :show, params: { id: @representative.id }
-      expect(assigns(:representative)).to eq(@representative)
-    end
-
-    it 'assigns the details of the representative to @details' do
-      allow(Representative).to receive(:get_representatives_by_ocdid).and_return(@officials)
-      get :show, params: { id: @representative.id }
-      expect(assigns(:details)).to eq(@officials[0])
-    end
-
-    it 'assigns @photoUrlExists to true if a photo_url exists in details of representative' do
-      allow(Representative).to receive(:get_representatives_by_ocdid).and_return(@officials)
-      get :show, params: { id: @representative.id }
-      expect(assigns(:photo_url_exists)).to be_truthy
-    end
-  end
 end
